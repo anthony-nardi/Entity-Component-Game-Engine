73435b110fc6b700ba70ccbcf7c138fdd2e8c370
Anthony Nardi <anthonygt@gmail.com>
2013-02-24 19:08:45 -0500

    tabs to spaces

--- entities/player.js
+++ entities/player.js
@@ -1,99 +1,99 @@
 moduleLoader.imports('player', ['unit', 'ec', 'mainView'], function (unit, ec, mainView) {
 
-	var player = ec(unit).extend({
-		
-		'position': {
-			'x': 0,
-			'y': 0
-		},
-		
-		'height': 20,
-		'width' : 20,
-		'color' : '#000000',
-		'speed' : 1.2,
-		'state' : {
-			'moving': false,
-			'render': true
-		},
-		'moveTo': {
-			'x': 0,
-			'y': 0
-		},
+  var player = ec(unit).extend({
+    
+    'position': {
+      'x': 0,
+      'y': 0
+    },
+    
+    'height': 20,
+    'width' : 20,
+    'color' : '#000000',
+    'speed' : 1.2,
+    'state' : {
+      'moving': false,
+      'render': true
+    },
+    'moveTo': {
+      'x': 0,
+      'y': 0
+    },
 
-		'handle': {
+    'handle': {
 
-			'click': function (event) {
-				
-				this.state.moving = true;
-			
-			}
+      'click': function (event) {
+        
+        this.state.moving = true;
+      
+      }
 
-		}
-	
-	})
+    }
+  
+  })
 
-	var move = function (event) {
+  var move = function (event) {
 
-		this.moveTo.x = mainView.getCurrentPointerPosition().x + mainView.scroll.x;
-		this.moveTo.y = mainView.getCurrentPointerPosition().y + mainView.scroll.y;
+    this.moveTo.x = mainView.getCurrentPointerPosition().x + mainView.scroll.x;
+    this.moveTo.y = mainView.getCurrentPointerPosition().y + mainView.scroll.y;
 
-		this.vector(this.position.x - this.moveTo.x, this.position.y - this.moveTo.y);
-		this.normalize();
-		this.scale(this.speed * mainView.zoom);
+    this.vector(this.position.x - this.moveTo.x, this.position.y - this.moveTo.y);
+    this.normalize();
+    this.scale(this.speed * mainView.zoom);
 
-		if (this.position.x === this.moveTo.x 
-		&& this.position.y === this.moveTo.y) {
-			
-			this.state.moving = false;
-			this.sub(this.vx, this.vy);
-			return;			
-		
-		}
-  	
-  	if (this.position.x !== this.moveTo.x) {
-  		if (Math.abs(Math.round(this.position.x - this.moveTo.x)) < this.speed) {
-  			this.position.x = this.moveTo.x;
-  		} else {
-  			this.position.x -= this.vx;
-  		}
-  	}
-  	
-  	if (this.position.y !== this.moveTo.y) {
-  		if (Math.abs(Math.round(this.position.y - this.moveTo.y)) < this.speed) {
-  			this.position.y = this.moveTo.y;
-  		} else {
-  			this.position.y -= this.vy;
-  		}
-  	}
+    if (this.position.x === this.moveTo.x 
+    && this.position.y === this.moveTo.y) {
+      
+      this.state.moving = false;
+      this.sub(this.vx, this.vy);
+      return;      
+    
+    }
+    
+    if (this.position.x !== this.moveTo.x) {
+      if (Math.abs(Math.round(this.position.x - this.moveTo.x)) < this.speed) {
+        this.position.x = this.moveTo.x;
+      } else {
+        this.position.x -= this.vx;
+      }
+    }
+    
+    if (this.position.y !== this.moveTo.y) {
+      if (Math.abs(Math.round(this.position.y - this.moveTo.y)) < this.speed) {
+        this.position.y = this.moveTo.y;
+      } else {
+        this.position.y -= this.vy;
+      }
+    }
   
   };
 
-	player.inputs();
-	
-	player.on('update', function () {
-		if (this.state.moving) {
-			move.call(this);
-		}
-	});
+  player.inputs();
+  
+  player.on('update', function () {
+    if (this.state.moving) {
+      move.call(this);
+    }
+  });
 
-	player.on('render', function () {
+  player.on('render', function () {
 
-		if (this.state.render) {
-			
-			var ctx = mainView.getContext();
-			
-			ctx.fillStyle = this.color;
-			
-			ctx.fillRect(
-				this.position.x - mainView.scroll.x, 
-				this.position.y - mainView.scroll.y, 
-				this.width * mainView.zoom, 
-				this.height * mainView.zoom
-			);
-		}
+    if (this.state.render) {
+      
+      var ctx = mainView.getContext();
+      
+      ctx.fillStyle = this.color;
+      
+      ctx.fillRect(
+        this.position.x - mainView.scroll.x, 
+        this.position.y - mainView.scroll.y, 
+        this.width * mainView.zoom, 
+        this.height * mainView.zoom
+      );
+    }
 
-	});
+  });
 
-	return player;
+  return player;
 
 });
--- system/vector.js
+++ system/vector.js
@@ -1,98 +1,98 @@
 moduleLoader.imports('vector', [], function () {
-	
-	var prototype = {
-	
-		'vector': function (x, y) {
-		
-			this.vx = x;
-			this.vy = y;
-		
-			return this;
-		
-		},
-	
-		'scale': function (magnitude) {
-		
-			this.vx *= magnitude;
-			this.vy *= magnitude;
-		
-			return this;
-		
-		},
-	
-		'add': function (vector) {
-		
-			this.vx += vector.vx;
-			this.vy += vector.vy;
-		
-			return this;
-		
-		},
-	
-		'sub': function (vector) {
-		
-			this.vx -= vector.vx;
-			this.vy -= vector.vy;
-		
-			return this;
+  
+  var prototype = {
+  
+    'vector': function (x, y) {
+    
+      this.vx = x;
+      this.vy = y;
+    
+      return this;
+    
+    },
+  
+    'scale': function (magnitude) {
+    
+      this.vx *= magnitude;
+      this.vy *= magnitude;
+    
+      return this;
+    
+    },
+  
+    'add': function (vector) {
+    
+      this.vx += vector.vx;
+      this.vy += vector.vy;
+    
+      return this;
+    
+    },
+  
+    'sub': function (vector) {
+    
+      this.vx -= vector.vx;
+      this.vy -= vector.vy;
+    
+      return this;
 
-		},
-	
-		'negate': function () {
-			
-			this.vx = -(this.vx);
-			this.vy = -(this.vy);
-			
-			return this;
-		
-		},
-	
-		'length': function () {
-	
-			return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-	
-		},
-	
-		'lengthSquared': function () {
-		
-			return this.vx * this.vx + this.vy * this.vy;
-		
-		},
-	
-		'normalize': function () {
-		
-			var len = this.length();
-		
-			if (len) {
-				this.vx /= len;
-				this.vy /= len;
-			}
-		
-			return this;
-		
-		},
-	
-		'rotate': function (angle) {
-		
-			var vx = this.vx,
-					vy = this.vy,
-					cosVal = Math.cos(angle),
-					sinVal = Math.sin(angle);
-			this.vx = vx * cosVal - vy * sinVal;
-			this.vy = vx * sinVal + vy * cosVal;
-		
-			return this
+    },
+  
+    'negate': function () {
+      
+      this.vx = -(this.vx);
+      this.vy = -(this.vy);
+      
+      return this;
+    
+    },
+  
+    'length': function () {
+  
+      return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+  
+    },
+  
+    'lengthSquared': function () {
+    
+      return this.vx * this.vx + this.vy * this.vy;
+    
+    },
+  
+    'normalize': function () {
+    
+      var len = this.length();
+    
+      if (len) {
+        this.vx /= len;
+        this.vy /= len;
+      }
+    
+      return this;
+    
+    },
+  
+    'rotate': function (angle) {
+    
+      var vx = this.vx,
+          vy = this.vy,
+          cosVal = Math.cos(angle),
+          sinVal = Math.sin(angle);
+      this.vx = vx * cosVal - vy * sinVal;
+      this.vy = vx * sinVal + vy * cosVal;
+    
+      return this
 
-		},
-	
-		'toString': function () {
-			return '(' + this.vx.toFixed(3) + ', ' + this.vy.toFixed(3) + ')';
-		}
-	
-	};
+    },
+  
+    'toString': function () {
+      return '(' + this.vx.toFixed(3) + ', ' + this.vy.toFixed(3) + ')';
+    }
+  
+  };
 
-	var vector = Object.create(prototype);
+  var vector = Object.create(prototype);
 
-	return vector;
+  return vector;
 
 });
